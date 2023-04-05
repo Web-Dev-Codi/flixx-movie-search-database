8c6aa70786c8d8ff1fb73eaebf0ebe90d893f0f1
Web-Dev-Robi <cordiscobrian@gmail.com>
2023-04-05 20:02:32 +0200

    details page

--- js/script.js
+++ js/script.js
@@ -65,6 +65,62 @@ async function displayPopularShows() {
     });
 }
 
+// Display Movie details
+async function displayMovieDetails() {
+    const movieID = window.location.search.split('=')[1];
+    
+
+    const movie = await fetchAPIData(`movie/${movieID}`)
+
+    const div = document.createElement('div');
+   
+    div.innerHTML = `
+         <div class="details-top">
+          <div>
+             ${movie.poster_path ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />` : `<img
+              src="images/no-image.jpg" 
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </div>
+          <div>
+            <h2>${movie.original_title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+             ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="#" target="_blank" class="btn">Visit Movie Homepage ${movie.homepage}</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${movie.budget}</li>
+            <li><span class="text-secondary">Revenue:</span> $${movie.revenue}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies[0].name}, ${movie.production_companies[0].name}, ${movie.production_companies[0].name}</div>
+        </div>`;
+
+    document.querySelector('#movie-details').appendChild(div);
+
+}
+
 
 
 // Fetch data from TMDB api
@@ -104,6 +160,10 @@ function activeLinks() {
     });
 }
 
+function addCommasToNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // Initalize App our own custom router using a switch statement
 function init() {
     switch (global.currentPage) {  // Created a instance for  our routing 
@@ -115,7 +175,7 @@ function init() {
             displayPopularShows();
             break;
         case "/movie-details.html":
-            console.log('Movie Details');
+            displayMovieDetails();
             break;
         case "/search.html":
             console.log('Search');
