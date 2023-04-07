3dc26ef45c6a0baa5d540fe04614fbd2b9ed6b25
Web-Dev-Robi <cordiscobrian@gmail.com>
2023-04-07 14:49:03 +0200

    Swiper

--- js/script.js
+++ js/script.js
@@ -37,12 +37,12 @@ async function displayPopularMovies() {
 // Display 20 Popular Shows
 async function displayPopularShows() {
   const { results } = await fetchAPIData('tv/popular');
-
+  console.log(results);
   results.forEach((shows) => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = ` 
-          <a href="tv-details.html?id=${shows.tv_id}">
+          <a href="tv-details.html?id=${shows.id}">
         ${shows.poster_path ? `<img
               src="https://image.tmdb.org/t/p/w500${shows.poster_path}"
               class="card-img-top"
@@ -65,118 +65,146 @@ async function displayPopularShows() {
   });
 }
 
-// Display Movie details
+// Display Movie Details
 async function displayMovieDetails() {
-  const movieID = window.location.search.split('=')[1];
-  const movie = await fetchAPIData(`movie/${movieID}`);
-  // Overlay for movie background
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  // Overlay for background image
   displayBackgroundImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
+
   div.innerHTML = `
-         <div class="details-top">
-          <div>
-             ${movie.poster_path ? `<img
-              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-              class="card-img-top"
-              alt="${movie.title}"
-            />` : `<img
-              src="images/no-image.jpg" 
-              class="card-img-top"
-              alt="${movie.title}"
-            />`
+  <div class="details-top">
+  <div>
+  ${movie.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
     }
-          </div>
-          <div>
-            <h2>${movie.original_title}</h2>
-            <p>
-              <i class="fas fa-star text-primary"></i>
-              ${movie.vote_average.toFixed(1)} / 10
-            </p>
-            <p class="text-muted">Release Date: ${movie.release_date}</p>
-            <p>
-             ${movie.overview}
-            </p>
-            <h5>Genres</h5>
-            <ul class="list-group">
-              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
-            </ul>
-            <a href="#" target="_blank" class="btn">Visit Movie Homepage ${movie.homepage}</a>
-          </div>
-        </div>
-        <div class="details-bottom">
-          <h2>Movie Info</h2>
-          <ul>
-            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
-            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
-            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
-            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
-          </ul>
-          <h4>Production Companies</h4>
-          <div class="list-group">${movie.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}</div>
-        </div>`;
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+      ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${movie.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${movie.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(', ')}
+  </div>
+</div>
+  `;
 
   document.querySelector('#movie-details').appendChild(div);
-
 }
 
-// Display Show details
+// Display Show Details
 async function displayShowDetails() {
-  const showID = window.location.search.split('=')[1];
-  const show = await fetchAPIData(`tv/${showID}`);
-  // Overlay for movie background
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overlay for background image
   displayBackgroundImage('tv', show.backdrop_path);
+
   const div = document.createElement('div');
+
   div.innerHTML = `
-         <div class="details-top">
-          <div>
-             ${show.poster_path ? `<img
-              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-              class="card-img-top"
-              alt="${show.name}"
-            />` : `<img
-              src="images/no-image.jpg" 
-              class="card-img-top"
-              alt="${show.name}"
-            />`
+  <div class="details-top">
+  <div>
+  ${show.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt="${show.name}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${show.name}"
+/>`
     }
-          </div>
-          <div>
-            <h2>${show.original_title}</h2>
-            <p>
-              <i class="fas fa-star text-primary"></i>
-              ${show.vote_average.toFixed(1)} / 10
-            </p>
-            <p class="text-muted">Release Date: ${show.last_air_date}</p>
-            <p>
-             ${show.overview}
-            </p>
-            <h5>Genres</h5>
-            <ul class="list-group">
-              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
-            </ul>
-            <a href="#" target="_blank" class="btn">Visit Movie Homepage ${show.homepage}</a>
-          </div>
-        </div>
-        <div class="details-bottom">
-          <h2>Movie Info</h2>
-          <ul>
-            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(show.budget)}</li>
-            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(show.revenue)}</li>
-            <li><span class="text-secondary">Runtime:</span> ${show.runtime} minutes</li>
-            <li><span class="text-secondary">Status:</span> ${show.status}</li>
-          </ul>
-          <h4>Production Companies</h4>
-          <div class="list-group">${show.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}</div>
-        </div>`;
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>
+      ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${show.homepage
+    }" target="_blank" class="btn">Visit show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes
+    }</li>
+    <li><span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.name
+    }</li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${show.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(', ')}
+  </div>
+</div>
+  `;
 
   document.querySelector('#show-details').appendChild(div);
-
 }
 
-// Display backdrop on details pages
+// Display Backdrop On Details Pages
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement('div');
-  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org./t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
   overlayDiv.style.backgroundSize = 'cover';
   overlayDiv.style.backgroundPosition = 'center';
   overlayDiv.style.backgroundRepeat = 'no-repeat';
@@ -192,8 +220,32 @@ function displayBackgroundImage(type, backgroundPath) {
     document.querySelector('#movie-details').appendChild(overlayDiv);
   } else {
     document.querySelector('#show-details').appendChild(overlayDiv);
-
   }
+}
+
+// Display Slider Movies
+async function diplaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average}/ 10
+            </h4>`;
+    
+    document.querySelector('.swiper-wrapper').appendChild(div)
+
+  });
+
+
+
+
 }
 
 
@@ -203,7 +255,7 @@ async function fetchAPIData(endpoint) {
   // Api Key
   const apiKey = '1b4e39380ba062c4708176b03ec88bd0';
   // Url to the endpoint
-  const api_URL = 'https://api.themoviedb.org/3';
+  const api_URL = 'https://api.themoviedb.org/3/';
   // Show Spinner on load of page
   showSpinner();
   // Initalizing fetch
@@ -244,6 +296,7 @@ function init() {
   switch (global.currentPage) {  // Created a instance for  our routing 
     case "/":
     case '/index.html':
+      diplaySlider();
       displayPopularMovies();
       break;
     case "/shows.html":
